0b608b5958727b9a82d28ce93f968c9fb517c4f3
Sabrina <sabrinalord@msn.com>
2022-02-03 15:54:36 +0000

    added tests to increase coverage

--- test/conjured_item_update.test.js
+++ test/conjured_item_update.test.js
@@ -1,6 +1,14 @@
 const {Shop, Item} = require("../src/gilded_rose");
 
 describe('Conjured Item', () => {
+
+  it('does not reduce in quality below 0', () => {
+    let items = new Item("Conjured Mana Cake", 1, 0);
+    const gildedRose = new Shop([items]);
+    gildedRose.updateQuality();
+    expect(gildedRose.items[0].quality).toBe(0);
+  })
+  
   it('degrades the quality of Conjured items by 2 after each daily update', () => {
     let items = new Item("Conjured Mana Cake", 5, 5);
     const gildedRose = new Shop([items]);
--- test/normal_item_update.test.js
+++ test/normal_item_update.test.js
@@ -2,6 +2,21 @@ const {Shop, Item} = require("../src/gilded_rose");
 
 
 describe('Normal Item', () => {
+
+  it('reduces quality by 1', () => {
+    let items = new Item("Test Item", 10, 5);
+    const gildedRose = new Shop([items]);
+    gildedRose.updateQuality();
+    expect(gildedRose.items[0].quality).toBe(4)  
+  })
+
+  it('reduces sellIn by 1 day after each update', () => {
+    let items = new Item("Test Item", 10, 5);
+    const gildedRose = new Shop([items]);
+    gildedRose.updateQuality();
+    expect(gildedRose.items[0].sellIn).toBe(9)  
+  })
+
   it('does not allow normal items to drop quality below 0', () => {
     let items = new Item("+5 Dexterity Vest", 10, 0);
     const gildedRose = new Shop([items]);
@@ -9,15 +24,20 @@ describe('Normal Item', () => {
     expect(gildedRose.items[0].quality).toBe(0)  
     });
 
-  it("reduces quality twice as fast once the sell by date has passed", () => {
+  it('reduces quality twice as fast once the sell by date has passed', () => {
     let items = new Item("+5 Dexterity Vest", 0, 4);
     const gildedRose = new Shop([items]);
     gildedRose.updateQuality();
     expect(gildedRose.items[0].quality).toBe(2)  
 
   });
+ 
+  it('reduces quality twice as fast once the sell by date has passed', () => {
+    let items = new Item("+5 Dexterity Vest", -1, 4);
+    const gildedRose = new Shop([items]);
+    gildedRose.updateQuality();
+    expect(gildedRose.items[0].quality).toBe(2)  
 
-  
-
+  });
 
 })
